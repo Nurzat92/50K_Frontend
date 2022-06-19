e2f6d140893abfcb813a1457a14a87a21d3ce800
Nurzat92 <107753690+Nurzat92@users.noreply.github.com>
2022-06-19 07:56:33 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//let name1 = 4;             //Urok23
+//console.log (name1);
+
+//age = 29;
+//console.log (age);
+
+//year = 1992;
+//console.log (year);
+
+//name1 = "Nurzat";
+//console.log (name1);
+
+//IsCalculate = 13;             //Urok26 zadanie1
+//console.log(IsCalculate-5);
+
+//IsCalculate = 13;
+//console.log(IsCalculate+5);
+
+//IsCalculate = 13;
+//console.log(IsCalculate*5);
+
+//IsCalculate = 13;
+//console.log(IsCalculate/5);
+
+//IsCalculate = 13;
+//console.log(IsCalculate%5);
+
+//var login = promt("Введите ваш логин и пароль","", "");
+ 
+if(true) {
+    alert("Welcome");
+} if (false); {
+    alert("Логин неправильный");
+} elif (false); {
+    alert("Пароль неправильный");
+    }
+
+
